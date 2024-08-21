5529e4e3f25590396d445744def272e6a2352b21
KrityaShah <118177316+KrityaShah@users.noreply.github.com>
2024-08-21 11:01:55 +0545

    Created Navbar
    created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import Navbar from "../components/Navbar"
+import { useState } from 'react' 
+
+
+
 function App() {
   
 
+  const [mode, setMode] = useState('light')
+  const [text, setText] = useState('dark')
+  
+  const toggleMode = () =>{
+    if(mode === 'light'){
+      setMode('dark')
+      setText('light')
+    }else{
+      setMode('light')
+      setText('dark')
+    }
+  }
+
   return (
     <>
-      
+      <Navbar mode={mode} text={text} toggleMode = {toggleMode}/>
     </>
   )
 }
