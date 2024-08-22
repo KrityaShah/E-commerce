f2c9d8433582ffda9908f984f18b45195b274ca1
KrityaShah <118177316+KrityaShah@users.noreply.github.com>
2024-08-22 23:34:54 +0545

    Changed the product's Design
    removed the old bootstrap of design of products then added new bootstrap design for products.

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -18,17 +18,30 @@ const Products = () => {
   return (
 
     <>
-    {products.map((product =>{
-        return <div key={product.id} className="productContainer border">
-            <div className="products">
+    {/* {products.map((product =>{
+        return <div key={product.id} classNameNameclassName="productContainer border">
+            <div classNameNameclassName="products">
                 <h4>Product id: {product.id}</h4>
                 <h5>Product name: {product.title}</h5>
                 <p>Product Description: {product.body}</p>
             </div>
         </div>
+    }))} */}
+
+{products.map((product =>{
+        return<div className="card">
+        <div className="card-body">
+          <h5 className="card-title">Product Name: {product.title}</h5>
+          <h6 className="card-subtitle mb-2 text-muted">Price: {product.price}</h6>
+          <h6 className="card-subtitle mb-2 text-muted">Rating: {product.rating}</h6>
+          <p className="card-text">Description: {product.description}</p>
+          {/* <a href="#" className="card-link">Card link</a>
+          <a href="#" className="card-link">Another link</a> */}
+        </div>
+      </div>
     }))}
   
-    
+  
     
     
     </>
