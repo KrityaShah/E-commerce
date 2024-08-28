7833b43b3f19fd183f2af2e97ab3b1afda5d6633
KrityaShah <118177316+KrityaShah@users.noreply.github.com>
2024-08-28 12:57:08 +0545

    Updated home, Carousel
    Update style of
    carousel,
    Home,
    and deleted unwanted images from assets

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,26 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import image1 from '../assets/image1.jpg';
 import image2 from '../assets/image2.jpg';
-import image3 from '../assets/image3.jpg';
 import image4 from '../assets/image4.jpg';
 
+const images = [image4, image2, image1];
 
 const Carousel = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
-    <div className="carousel-inner">
-      <div className="carousel-item active">
-        <img className="d-block w-100" src={image4} alt="First slide"/>
-      </div>
-      <div className="carousel-item">
-        <img className="d-block w-100" src={image2} alt="Second slide"/>
-      </div>
-      <div className="carousel-item">
-        <img className="d-block w-100" src={image3} alt="Third slide"/>
+    <div 
+      style={{ 
+        display: 'flex', 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        height: '80vh',
+        marginTop: '40px', 
+      }}
+    >
+      <div style={{ width: '90vw', overflow: 'hidden' }}>
+        <img
+          className="d-block w-100"
+          src={images[currentIndex]}
+          alt={`Slide ${currentIndex + 1}`}
+          style={{ width: '100%', height: '400px', objectFit: 'cover' }}
+        />
       </div>
     </div>
-  </div>
-  )
+  );
 }
 
-export default Carousel
+export default Carousel;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,16 @@
-import React from 'react'
-import Carousel from './Carousel'
+import React from 'react';
+import Carousel from './Carousel';
+import "./Home.css"; 
 
 const Home = () => {
   return (
-    <>
-    <div>
-        <h2>This is home!</h2>
-
-      <Carousel/>
-      
-      
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
+      <h1 style={{ marginTop: '150px', fontSize:'4.5rem'}}>Welcome to Ecommerce!</h1>
+      <p style={{fontSize:'2.5rem'}}>Shop anywhere, anytime</p>
+      <button className="btn btn-outline-success" style={{ margin: '20px' }}>Shop now</button>
+      <Carousel />
     </div>
-    </>
-  )
+  );
 }
 
-export default Home
+export default Home;
