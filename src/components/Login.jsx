5333b5308efe8d73918c4188510dc68b483d37af
KrityaShah <118177316+KrityaShah@users.noreply.github.com>
2024-08-28 18:05:06 +0545

    updated login component
    with styling, and added class to div in error of user and password

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useForm  } from "react-hook-form"
+import "./Login.css"
 
 
 const Login = () => {
@@ -14,10 +15,11 @@ const Login = () => {
 
   return (
     <div className='container'>
-    
+        
     <form action="" onSubmit={handleSubmit(onSubmit)}>
+    <h1>LOGIN</h1>
     <input placeholder='Username' {...register("username", { required: true, minLength: 3, maxLength:15 })} type="text" />
-    {errors.username && <div>There is error in username</div> }
+    {errors.username && <div className='error'>There is error in username</div> }
     <input placeholder='Password' {...register("password", { required: true, minLength: 8, maxLength:15 })} type="password"  />
     {errors.username && <div>There is error in password</div> }
     <input type="submit" value="submit" />
