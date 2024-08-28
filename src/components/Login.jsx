ecb1f27eb6c5afab176f41fd3e60a64df1bd2a4d
KrityaShah <118177316+KrityaShah@users.noreply.github.com>
2024-08-28 17:48:25 +0545

    Updated login form
    creaated form with react hook form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { useForm  } from "react-hook-form"
+
 
 const Login = () => {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+      } = useForm()
+
+      const onSubmit = (data) => console.log(data)
+
   return (
-    <div>
-      
+    <div className='container'>
+    
+    <form action="" onSubmit={handleSubmit(onSubmit)}>
+    <input placeholder='Username' {...register("username", { required: true, minLength: 3, maxLength:15 })} type="text" />
+    {errors.username && <div>There is error in username</div> }
+    <input placeholder='Password' {...register("password", { required: true, minLength: 8, maxLength:15 })} type="password"  />
+    {errors.username && <div>There is error in password</div> }
+    <input type="submit" value="submit" />
+    </form>
     </div>
   )
 }
