9b02b8ae60118a477e9ec65b9fbfd995e7a7d597
KrityaShah <118177316+KrityaShah@users.noreply.github.com>
2024-08-28 19:37:42 +0545

    Updated login
    updated login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -8,7 +8,8 @@ const Login = () => {
         register,
         handleSubmit,
         watch,
-        formState: { errors },
+        
+        formState: { errors, isSubmitting },
       } = useForm()
 
       const onSubmit = (data) => console.log(data)
@@ -18,11 +19,11 @@ const Login = () => {
         
     <form action="" onSubmit={handleSubmit(onSubmit)}>
     <h1>LOGIN</h1>
-    <input placeholder='Username' {...register("username", { required: true, minLength: 3, maxLength:15 })} type="text" />
-    {errors.username && <div className='error'>There is error in username</div> }
-    <input placeholder='Password' {...register("password", { required: true, minLength: 8, maxLength:15 })} type="password"  />
-    {errors.username && <div className='error'>There is error in password</div> }
-    <input type="submit" value="submit" />
+    <input placeholder='Username' {...register("username", { required: {value: true, message: 'Please fill'}, minLength: {value: 3, message : 'Min length is 3'}, maxLength: {value: 10, message : 'Max length is 10' }})} type="text" />
+    {errors.username && <div className='error'>{errors.username.message}</div> }
+    <input placeholder='Password' {...register("password", { required: {value: true, message:'Please fill'}, minLength: 8, maxLength:15 })} type="password"  />
+    {errors.password && <div className='error'>{errors.password.message}</div> }
+    <input disabled={isSubmitting} type="submit" value="submit" />
     </form>
     </div>
   )
