a64529a60a99972fb8979fec75a9c4693e049892
KrityaShah <118177316+KrityaShah@users.noreply.github.com>
2024-08-30 11:52:23 +0545

    Created Footer
    Footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+    <p>&copy; 2024 ECommerce Company. All rights reserved.</p>
+    <p>
+      Follow us on
+      <a href="https://twitter.com">Twitter</a>,
+      <a href="https://facebook.com">Facebook</a>, and
+      <a href="https://instagram.com">Instagram</a>.
+    </p>
+  </footer>
+  )
+}
+
+export default Footer
