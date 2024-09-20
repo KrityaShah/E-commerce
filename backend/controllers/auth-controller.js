3b4b5d7ca8aed2c719a1608520d3cf7eff918550
KrityaShah <118177316+KrityaShah@users.noreply.github.com>
2024-09-21 00:23:02 +0545

    Registed Stored in database and encryypted

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -1,3 +1,6 @@
+const User = require("../model/user-model");
+const bcrypt = require('bcryptjs');
+
 // For home:
 
 const home = async (req, res) =>{
@@ -15,9 +18,24 @@ const home = async (req, res) =>{
 // for register
 const register = async (req, res) => {
     try {
+
+        const { username, email, password, phone } = req.body;
+
+        const userExist = await User.findOne({email});
+
+        if(userExist){
+            return res.status(400).json({message: "Email already exist"})
+        }
+
+
+        const saltRound = 10;
+        const hash_password = await bcrypt.hash(password, saltRound)
+
+        const userCreated = await User.create({username, email, password: hash_password, phone});
+
         res
         .status(201)
-        .json({message: req.body});
+        .json({message:"Register Sucessfull"});
         
     } catch (error) {
         console.error(error);
